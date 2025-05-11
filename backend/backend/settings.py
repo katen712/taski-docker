cc8dc5c21f97998ff52e7b9587047f15ffb3f4ca
Ekaterina Smirnova <katen712@yandex.ru>
2025-05-11 12:49:36 +0300

    restore to working

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,18 +1,14 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key_for_tests')
 
-DEBUG = (os.getenv('DEBUG') == 'True')
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+DEBUG = True
+
+ALLOWED_HOSTS = ['51.250.97.134', '127.0.0.1', 'localhost', 'https://kittygram.servepics.com']
 
 
 # Application definition
